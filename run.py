63b5c4599a1028ad210e958cc73aba87b8631996
Kevin Shook <shookke@gmail.com>
2018-07-10 17:30:43 -0400

    need to push for testing

--- run.py
+++ run.py
@@ -27,8 +27,8 @@ def process_imu(quat, acc, gyro):
 def process_sync(arm, x_direction):
     if arm == 255:
         temp_thread.stop()
-    if arm == 2 or 1:
-        temp_thread.start()
+    #if arm == 2 or 1:
+        #temp_thread.start()
     print(arm, x_direction)
 
 def process_classifier(pose):
@@ -85,7 +85,7 @@ myo_device.services.classifier_notifications()
 myo_device.services.set_mode(myo.EmgMode.FILT, myo.ImuMode.DATA, myo.ClassifierMode.ON)
 # myo_device.add_emg_event_handler(process_emg)
 # myo_device.add_emg_event_handler(led_emg)
-myo_device.add_imu_event_handler(process_imu)
+#myo_device.add_imu_event_handler(process_imu)
 myo_device.add_sync_event_handler(process_sync)
 myo_device.add_classifier_event_hanlder(process_classifier)
 
