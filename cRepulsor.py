df7bd15142d543c9bf5cc1b8c6db1a4fcc2a2ab3
Kevin Shook <shookke@gmail.com>
2018-07-11 19:37:59 -0400

    fixing repulsor animation

--- cRepulsor.py
+++ cRepulsor.py
@@ -4,7 +4,7 @@ import time
 from random import random
 
 pygame.mixer.init()
-pygame.mixer.music.set_volume(1.0)
+
 
 class cRepulsor:
     def __init__(self, pwm, led):
@@ -22,33 +22,40 @@ class cRepulsor:
 
     def arm(self):
         self.pwm.set_pwm_freq(1000)
-        pygame.mixer.music.load(self.arming)   
+        pygame.mixer.music.load(self.arming)  
+        pygame.mixer.music.set_volume(1.0) 
         pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            for i in range(self.LED_min, self.LED_med):
+        time.sleep(0.5)
+        for i in range(self.LED_min, self.LED_med):
                 #print(i)
                 self.pwm.set_pwm(self.led, 0, i)
+                time.sleep(0.2)
+        while pygame.mixer.music.get_busy() == True:
             continue
         self.armed = True
     
     def disarm(self):
         self.pwm.set_pwm_freq(1000)
-        pygame.mixer.music.load(self.disarming)   
+        pygame.mixer.music.load(self.disarming)
+        pygame.mixer.music.set_volume(1.0)   
         pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy() == True:
-            for i in range(self.LED_med, -1, -1):
+        time.sleep(.5)
+        for i in range(self.LED_med, -1, -1):
                         #print(i)
                         self.pwm.set_pwm(self.led, 0, i)
+                        time.sleep(0.2)
+        while pygame.mixer.music.get_busy() == True:
             continue
         self.armed = False
     
     def fire(self):
         if self.armed:
             pygame.mixer.music.load(self.disarming)   
+            pygame.mixer.music.set_volume(1.0)
             pygame.mixer.music.play()
+            time.sleep(0.5)
+            self.pwm.set_pwm(4, 0, self.LED_max)
             while pygame.mixer.music.get_busy() == True:
-                self.pwm.set_pwm(4, 0, self.LED_max)
-                time.sleep(0.5)
                 continue
             self.arm()
     
