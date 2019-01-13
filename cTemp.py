8cf2e06acabc3228869b79807ab74b6e96486bf6
Kevin <shookke@gmail.com>
2019-01-12 19:15:59 -0500

    Missed a call

--- cTemp.py
+++ cTemp.py
@@ -5,10 +5,10 @@ class cTemp:
     def __init__(self, pwm, cooler, *args, **kwargs):
         self.pwm = pwm
         self.cooler = cooler
-        self.pwm_freq = self.pwm.set_pwm_freq(60)
+        self.pwm.frequency = 60
 
     def cool(self, temp):
-        self.pwm.set_pwm_freq(60)
+        self.pwm.frequency = 60
         while True:
             self.pwm.set_pwm(1, 1, 0)
             time.sleep(temp)
