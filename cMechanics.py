cf3a36d54b3e1a55975b1a69fb7dc871af8b6a07
Kevin <shookke@gmail.com>
2019-01-13 21:26:39 -0500

    battery status indication from Myo

--- cMechanics.py
+++ cMechanics.py
@@ -6,12 +6,12 @@ class cMechanics:
 
     def __init__(self, **kwargs):
         ORDER = neopixel.RGBW
-        self.leds = neopixel.NeoPixel(board.D21, 2, pixel_order=ORDER)
+        self.leds = neopixel.NeoPixel(board.D12, 2, pixel_order=ORDER)
         for key in kwargs:
             setattr(self, key, kwargs[key])
         
     def boot(self):
-        self.leds[0] = (0, 0, 0, 150)
+        return
     
     def status(self, batt, charging):
         if batt <= 20:
@@ -19,9 +19,9 @@ class cMechanics:
         if charging:
             while batt != 100:
                 for i in range(0, 100):
-                    self.leds.fill((i, 0, 0, 0))
+                    self.leds.fill((0, i, 0, 0))
                 for i in range(100, 0, -1):
-                    self.leds.fill((i, 0, 0, 0))    
+                    self.leds.fill((0, i, 0, 0))    
         else:
-            self.leds.fill((0, 0, 0, 150))
+            self.leds.fill((0, 0, 100, 150))
 
